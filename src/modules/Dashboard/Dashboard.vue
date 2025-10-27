870f6e04dd7f0418899f2461e58942d2aed0c822
Thorsten Klein <tk@thklein.io>
2025-10-27 05:58:16 +0100

    fix: version display string

--- src/modules/Dashboard/Dashboard.vue
+++ src/modules/Dashboard/Dashboard.vue
@@ -131,7 +131,7 @@
             <div class="info-grid">
                 <div class="info-item">
                     <span class="info-label">{{ t('souvera_central', 'Version') }}:</span>
-                    <span class="info-value">0.4.0</span>
+                    <span class="info-value">0.5.0</span>
                 </div>
                 <div class="info-item">
                     <span class="info-label">{{ t('souvera_central', 'Erlaubte Domains') }}:</span>
